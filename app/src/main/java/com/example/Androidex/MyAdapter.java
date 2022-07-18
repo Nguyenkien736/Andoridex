7db9a4948b745b7bd4392b15e57c5f09d1b7c349
Nguyenkien736 <72335258+Nguyenkien736@users.noreply.github.com>
2022-07-18 23:32:06 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/Androidex/MyAdapter.java
@@ -0,0 +1,150 @@
+package com.example.Androidex;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+    Context context;
+    private MaterialAlertDialogBuilder dialogBuilder;
+    private EditText newFileName;
+    private Button cancelButton, saveBUtton;
+    private Dialog dialog;
+
+    File[] filesAndFolders;
+
+    public MyAdapter(Context context, File[] filesAndFolders){
+        this.context = context;
+        this.filesAndFolders = filesAndFolders;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+
+        File selectedFile = filesAndFolders[position];
+        holder.textView.setText(selectedFile.getName());
+
+        if(selectedFile.isDirectory()){
+            holder.imageView.setImageResource(R.drawable.ic_baseline_folder_24);
+        }else{
+            holder.imageView.setImageResource(R.drawable.ic_baseline_insert_drive_file_24);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedFile.isDirectory()){
+                    Intent intent = new Intent(context, FileListActivity.class);
+                    String path = selectedFile.getAbsolutePath();
+                    intent.putExtra("path",path);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                }else{
+                    //open thte file
+                    try {
+                        Intent intent = new Intent();
+                        intent.setAction(android.content.Intent.ACTION_VIEW);
+                        String type = "image/*";
+                        intent.setDataAndType(Uri.parse(selectedFile.getAbsolutePath()), type);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(intent);
+                    }catch (Exception e){
+                        Toast.makeText(context.getApplicationContext(),"Cannot open the file",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                PopupMenu popupMenu = new PopupMenu(context,v);
+                popupMenu.getMenu().add("DELETE");
+                popupMenu.getMenu().add("MOVE");
+                popupMenu.getMenu().add("RENAME");
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if(item.getTitle().equals("DELETE")){
+                            boolean deleted = selectedFile.delete();
+                            if(deleted){
+                                Toast.makeText(context.getApplicationContext(),"DELETED ",Toast.LENGTH_SHORT).show();
+                                v.setVisibility(View.GONE);
+                            }
+                        }
+                        if(item.getTitle().equals("MOVE")){
+                            createLayoutInflater();
+
+                        }
+                        if(item.getTitle().equals("RENAME")){
+                            createLayoutInflater();
+                        }
+                        return true;
+                    }
+                });
+
+                popupMenu.show();
+                return true;
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return filesAndFolders.length;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+        ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.file_name_text_view);
+            imageView = itemView.findViewById(R.id.icon_view);
+        }
+    }
+    public void createLayoutInflater(){
+        dialogBuilder = new MaterialAlertDialogBuilder(context);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View renameView = layoutInflater.inflate(R.layout.rename,null);
+        newFileName = (EditText) renameView.findViewById(R.id.newName);
+        cancelButton = (Button) renameView.findViewById(R.id.cacel_rename_button);
+        saveBUtton = (Button)  renameView.findViewById(R.id.renameButton);
+        final Dialog dialog1 = new Dialog(context);
+        dialog1.setContentView(renameView);
+        dialog1.show();
+    }
+}
